76f59c0c973871830d4df94b9609ee2df09eb817
matteo <m.pedone2000@gmail.com>
2023-05-16 04:46:07 -0400

    cbc attack

--- python1/cryptography-03lpyov-exercises/AY2223/Python/attacks/CBCPaddingOracle/CBC_PaddingOracle_Attack_video.py
+++ python1/cryptography-03lpyov-exercises/AY2223/Python/attacks/CBCPaddingOracle/CBC_PaddingOracle_Attack_video.py
@@ -10,8 +10,8 @@ from mydata import cbc_oracle_ciphertext as ciphertext
 from Crypto.Cipher import AES
 
 if __name__ == '__main__':
-    # server = remote(HOST,PORT)
-    # server.send(iv)
+    # server = remote(HOST,PORT)            #PROVARE QUESTE RIGHE COMMENTATE
+    # server.send(iv)                       #SONO DELLE PROVE PER VEDERE IL SERVER QUALE RISPOSTA FORNISCE PER DETERMINATI INPUT CHE RICEVE
     # server.send(ciphertext)
     # response = server. recv(1024)
     # print(response)
@@ -29,14 +29,14 @@ if __name__ == '__main__':
     # server.close()
 
 #---------------------------------------------
-    print(len(ciphertext)//AES.block_size)
+    print(len(ciphertext)//AES.block_size)  #IN QUANTI BLOCCHI AES SI PUÒ DIVIDERE IL CIPHERTEXT
     N = len(ciphertext)//AES.block_size
-    initial_part = ciphertext[:(N-2)*AES.block_size]
-    block_to_modify = bytearray(ciphertext[(N-2)*AES.block_size:(N-1)*AES.block_size])
-    last_block = ciphertext[(N-1)*AES.block_size:]
+    initial_part = ciphertext[:(N-2)*AES.block_size]    #DALL'INIZIO DEL BLOCCO AL BLOCCO N-2 DEL CIPHERTECT
+    block_to_modify = bytearray(ciphertext[(N-2)*AES.block_size:(N-1)*AES.block_size])  #PUNULTIMO BLOCCO
+    last_block = ciphertext[(N-1)*AES.block_size:]  #ULTIMO BLOCCO
 
 
-    byte_index = AES.block_size - 1
+    byte_index = AES.block_size - 1 #INDICE DEL BLOCCO DA MODIFICARE
     c_15 = block_to_modify[byte_index]
 
     for c_prime_15 in range(256):
@@ -51,13 +51,13 @@ if __name__ == '__main__':
         server.close()
 
         if response == b'OK':
-            print("c_prime_15="+str(c_prime_15))
+            print("c_prime_15="+str(c_prime_15))    #VALORE CHE CI HA FATTO OTTENERE OK DALL'ORACLE
             p_prime_15 = c_prime_15 ^ 1
             p_15 = p_prime_15 ^ c_15
             print("p_prime_15=" + str(p_prime_15))
             print("p_15=" + str(p_15))
 
-    p_prime_15 = 191
+    p_prime_15 = 191    #UNO DEI VALORI CHE CI HA FATTO OTTENERE OK DALL'ORACLE
     print("---------------")
 
     c_second_15 = p_prime_15 ^ 2
